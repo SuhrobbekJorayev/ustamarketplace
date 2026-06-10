f60666dad654d644ed00c82bf9b4921e1a8a29bc
Suhrobbek Jo'rayev <suhrobbek.jorayev0718>
2026-06-10 21:22:43 +0500

    change title to DEMO PROJECT

--- config/endpoints.js
+++ config/endpoints.js
@@ -1,5 +1,5 @@
 // Frontend/config/endpoints.js
-const BASE_URL = "http://127.0.0.1:8000/api/v1";
+const BASE_URL = "https://ustamarketplace.onrender.com/api/v1";
 
 async function getAuthHeaders() {
     let token = localStorage.getItem("access");
