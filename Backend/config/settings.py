e4303b698c7e0a6fbebf10745f4c848099c6e92e
Suhrobbek Jo'rayev <suhrobbek.jorayev0718>
2026-06-10 15:41:54 +0500

    Configure PostgreSQL database

--- Backend/config/settings.py
+++ Backend/config/settings.py
@@ -15,7 +15,6 @@ from datetime import timedelta
 from pathlib import Path
 from dotenv import load_dotenv
 
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -84,10 +83,22 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
     }
 }
 
@@ -155,4 +166,3 @@ REST_FRAMEWORK = {
         'rest_framework.filters.SearchFilter'
     ]
 }
-
--- Backend/jobs/admin.py
+++ Backend/jobs/admin.py
@@ -8,7 +8,3 @@ admin.site.register(Review)
 admin.site.register(Service)
 admin.site.register(User)
 admin.site.register(WorkerProfile)
-# {
-#   "refresh": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoicmVmcmVzaCIsImV4cCI6MTc4MDkzNjM2OSwiaWF0IjoxNzgwODQ5OTY5LCJqdGkiOiJlZWU4OWVkYzlmNmE0ODg2YWRjZTk5N2QzODZlMGUxYSIsInVzZXJfaWQiOiI0In0.VkjGnNcYR-49Y8RzzzTS_aQPlclS4HQaC4jWiDTStQc",
-#   "access": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzgwODUwMjY5LCJpYXQiOjE3ODA4NDk5NjksImp0aSI6ImFkNDQxNGRjNDU1NjRhNGU4NzRkY2ZjNDlhM2ZjNmEyIiwidXNlcl9pZCI6IjQifQ.Fr0oz2z41SOWHnnqOHn4XKkBLaJR3MKQq8yHz8Fiid0"
-# }
