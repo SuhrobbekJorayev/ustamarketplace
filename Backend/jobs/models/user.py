b57c9f30159dd96c54595ee73e213f4ddb4e5601
Suhrobbek Jo'rayev <suhrobbek.jorayev0718>
2026-06-11 11:10:07 +0500

    add user profile view and edit functionality

--- Backend/jobs/models/user.py
+++ Backend/jobs/models/user.py
@@ -18,3 +18,8 @@ class User(AbstractUser):
         max_length=20,
         unique=True
     )
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.username
