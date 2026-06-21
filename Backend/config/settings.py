2687311546d6a7ec98b7983e7964e81cb429490d
Suhrobbek Jo'rayev <suhrobbek.jorayev0718>
2026-06-21 11:26:30 +0500

    fix: readme

--- Backend/config/settings.py
+++ Backend/config/settings.py
@@ -171,17 +171,17 @@ REST_FRAMEWORK = {
     ]
 }
 
-# Email
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-
-EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
-
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+# # Email
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+#
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_PORT = 587
+# EMAIL_USE_TLS = True
+#
+# EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+# EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+#
+# DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 # Celery
 # CELERY_BROKER_URL = os.getenv('CELERY_BROKER_URL')
