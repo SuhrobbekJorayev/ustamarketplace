cb3d2e6704eb516773d32f6f98b43d6a95684fa0
Suhrobbek Jo'rayev <suhrobbek.jorayev0718>
2026-07-13 14:39:20 +0500

    test

---
+++ Backend/jobs/tests/test_auth.py
@@ -0,0 +1,13 @@
+# from rest_framework.test import APITestCase
+#
+#
+# class test_register_client_success(APITestCase):
+#     data = {
+#         'username': 'John',
+#         'email': 'john@gmail.com',
+#         'phone_number': '+998 91 289 49 10',
+#         'role': 'client',
+#         'password': 'john123'
+#     }
+#
+#     response =
