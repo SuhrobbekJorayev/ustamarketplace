6c3c21af8bd40df1cfdcb04d8a448880e9d61d7b
Suhrobbek Jo'rayev <suhrobbek.jorayev0718>
2026-07-18 19:03:38 +0500

    test: send email with multithreading

--- Backend/jobs/signals.py
+++ Backend/jobs/signals.py
@@ -1,5 +1,8 @@
+import threading
+
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+
 from jobs.models import Order, User, WorkerProfile
 from jobs.tasks import send_order_notification_mail
 
@@ -11,13 +14,17 @@ def order_created(sender, instance, created, **kwargs):
 
     worker_user = instance.service.worker
 
-    send_order_notification_mail(
-        to_email=worker_user.email,
-        worker_username=worker_user.username,
-        order_id=instance.id,
-        client_username=instance.client.username,
-        service_name=instance.service.name
-    )
+    threading.Thread(
+        target=send_order_notification_mail,
+        args=(
+            worker_user.email,
+            worker_user.username,
+            instance.id,
+            instance.client.username,
+            instance.service.name
+        ),
+        daemon=True
+    ).start()
 
 
 @receiver(post_save, sender=User)
