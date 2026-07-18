a628eb93e0ce579f111e99ed2e8e8a57bf450be3
Suhrobbek Jo'rayev <suhrobbek.jorayev0718>
2026-07-18 18:35:51 +0500

    test: order_created log

--- Backend/jobs/signals.py
+++ Backend/jobs/signals.py
@@ -6,10 +6,16 @@ from jobs.tasks import send_order_notification_mail
 
 @receiver(post_save, sender=Order)
 def order_created(sender, instance, created, **kwargs):
+    print("1. Signal ishga tushdi")
+
     if not created:
         return
 
+    print("2. created=True")
+
     worker_user = instance.service.worker
+    print("3. Worker:", worker_user.username)
+    print("4. Email:", worker_user.email)
 
     send_order_notification_mail(
         to_email=worker_user.email,
@@ -19,8 +25,12 @@ def order_created(sender, instance, created, **kwargs):
         service_name=instance.service.name
     )
 
+    print("5. Email yuborildi")
+
 
 @receiver(post_save, sender=User)
 def create_worker_profile(sender, instance, created, **kwargs):
     if created and instance.role == 'worker':
         WorkerProfile.objects.create(user=instance)
+
+print("1. Signal ishga tushdi")
