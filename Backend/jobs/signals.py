cd5740845c340037f85188e90c4c8a3eff13ca9d
Suhrobbek Jo'rayev <suhrobbek.jorayev0718>
2026-07-18 17:57:33 +0500

    feat: notify workers by email on new orders

--- Backend/jobs/signals.py
+++ Backend/jobs/signals.py
@@ -1,28 +1,23 @@
-# from django.db.models.signals import post_save
-# from django.dispatch import receiver
-# from jobs.models import Order
-# from jobs.tasks import send_order_notification_mail
-#
-#
-# @receiver(post_save, sender=Order)
-# def order_created(sender, instance, created, **kwargs):
-#     if not created:
-#         return
-#
-#     worker_user = instance.service.worker
-#
-#     send_order_notification_mail(
-#         to_email=worker_user.email,
-#         worker_username=worker_user.username,
-#         order_id=instance.id,
-#         client_username=instance.client.username,
-#         service_name=instance.service.name
-#     )
-# # email completed, now give it a try
-
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from jobs.models import User, WorkerProfile
+from jobs.models import Order, User, WorkerProfile
+from jobs.tasks import send_order_notification_mail
+
+
+@receiver(post_save, sender=Order)
+def order_created(sender, instance, created, **kwargs):
+    if not created:
+        return
+
+    worker_user = instance.service.worker
+
+    send_order_notification_mail(
+        to_email=worker_user.email,
+        worker_username=worker_user.username,
+        order_id=instance.id,
+        client_username=instance.client.username,
+        service_name=instance.service.name
+    )
 
 
 @receiver(post_save, sender=User)
