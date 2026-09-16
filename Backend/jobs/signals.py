d1d4abb1bb466336e03155d2e08b5839d74e86b1
Suhrobbek Jo'rayev <suhrobbek.jorayev0718>
2026-09-16 18:46:25 +0500

    fix: added auto create worker profile

--- Backend/jobs/signals.py
+++ Backend/jobs/signals.py
@@ -1,6 +1,8 @@
-# from django.db.models.signals import post_save
-# from django.dispatch import receiver
-# from jobs.models import Order, User, WorkerProfile
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from jobs.models import Order, User, WorkerProfile
+
+
 # from jobs.tasks import send_order_notification_mail
 #
 #
@@ -19,8 +21,8 @@
 #         service_name=instance.service.name
 #     )
 #
-#
-# @receiver(post_save, sender=User)
-# def create_worker_profile(sender, instance, created, **kwargs):
-#     if created and instance.role == 'worker':
-#         WorkerProfile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def create_worker_profile(sender, instance, created, **kwargs):
+    if created and instance.role == 'worker':
+        WorkerProfile.objects.create(user=instance)
